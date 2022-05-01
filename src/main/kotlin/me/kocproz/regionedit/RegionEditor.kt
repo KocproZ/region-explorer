9d8b818a6861cf10d8fa407d0dfbfb794b7493c0
KocproZ <kocproz@pm.me>
2022-05-01 04:14:40 +0200

    Some rendering code

--- src/main/kotlin/me/kocproz/regionedit/RegionEditor.kt
+++ src/main/kotlin/me/kocproz/regionedit/RegionEditor.kt
@@ -8,10 +8,23 @@ import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import me.kocproz.regionedit.ui.tree.TreeElement
 import me.kocproz.regionedit.ui.tree.tree
+import net.benwoodworth.knbt.NbtByte
+import net.benwoodworth.knbt.NbtByteArray
+import net.benwoodworth.knbt.NbtCompound
+import net.benwoodworth.knbt.NbtDouble
+import net.benwoodworth.knbt.NbtFloat
+import net.benwoodworth.knbt.NbtInt
+import net.benwoodworth.knbt.NbtIntArray
+import net.benwoodworth.knbt.NbtList
+import net.benwoodworth.knbt.NbtLong
+import net.benwoodworth.knbt.NbtLongArray
+import net.benwoodworth.knbt.NbtShort
+import net.benwoodworth.knbt.NbtString
+import net.benwoodworth.knbt.NbtTag
 import kotlin.io.path.Path
 
 fun main(args: Array<String>) {
-    val regionFile = Path("data/r.-10.-9.mca")
+    val regionFile = Path("data/entities/r.-10.-9.mca")
 
     val region = RegionFile(regionFile.toFile())
     var chunks: List<Chunk> = mutableListOf()
@@ -24,7 +37,7 @@ fun main(args: Array<String>) {
     runApplication(chunks)
 }
 
-fun runApplication(chunk: List<Chunk>) = application {
+fun runApplication(chunks: List<Chunk>) = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "Region Viewer",
@@ -32,13 +45,31 @@ fun runApplication(chunk: List<Chunk>) = application {
     ) {
         MaterialTheme {
             val elements = remember {
-                (1..100).map { parent ->
+                chunks.map { parent ->
                     TreeElement(
                         "$parent",
-                        (1..2).map { TreeElement("$parent-$it", listOf(TreeElement("$parent-$it-1"))) })
+                        parseNbtTag(parent.nbtData?.get(""))
+                    )
                 }
             }
             tree(elements)
         }
     }
 }
+
+fun parseNbtTag(tag: NbtTag?): List<TreeElement> =
+    when (tag) {
+        is NbtInt           -> listOf(TreeElement("I: ${tag.value}"))
+        is NbtByte          -> listOf(TreeElement("B: ${tag.value}"))
+        is NbtLong          -> listOf(TreeElement("L: ${tag.value}"))
+        is NbtFloat         -> listOf(TreeElement("F: ${tag.value}"))
+        is NbtDouble        -> listOf(TreeElement("D: ${tag.value}"))
+        is NbtShort         -> listOf(TreeElement("S: ${tag.value}"))
+        is NbtString        -> listOf(TreeElement("T: ${tag.value}"))
+        is NbtCompound      -> tag.entries.map { TreeElement(it.key, parseNbtTag(it.value)) }
+        is NbtList<NbtTag>  -> tag.mapIndexed { index, it -> TreeElement("$index", parseNbtTag(it)) }
+        is NbtIntArray      -> tag.mapIndexed { index, it -> TreeElement("I[$index]: $it") }
+        is NbtByteArray     -> tag.mapIndexed { index, it -> TreeElement("B[$index]: $it") }
+        is NbtLongArray     -> tag.mapIndexed { index, it -> TreeElement("L[$index]: $it") }
+        else                -> listOf(TreeElement("Idk lol, ${tag?.javaClass}"))
+    }
